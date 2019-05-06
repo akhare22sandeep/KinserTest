69b38c95c1bfab8dc673c986287844ca72ebeed6
Sandeep Akhare <sandeepa@danlawinc.com>
2019-05-05 22:39:59 -0400

    missing files

---
+++ KinserLib/Constants.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinserLib
+{
+	public class Constants
+	{
+
+
+		public static IDictionary<string, string> TitleMap()
+		{
+			Dictionary<string, string> TitleAndPathDic = new Dictionary<string, string>
+		{
+
+			{"Risk Assessment", "//div[@id='oasisImmunizations']//table[@id='mainTable']" },
+			{"Prognosis", "//div[@id='advancedCarePlan']"},
+			{"Sensory Status", "//table[@id='mainTable' and tbody//tr/th[@class='textCenter bold' and contains(text(), 'Sensory Status')]]"},
+			{"Pain", "//table[@id='mainTable' and tbody//tr/th[@class='textCenter bold' and contains(text(), 'Pain Scale')]]"},
+			{"Integumentary Status", "//table[@id='mainTable' and tbody//tr/th[@class='textCenter bold' and contains(text(), 'Integumentary Status')]]"},
+			{"Respiratory Status", "//table[@id='mainTable' and tbody//tr/th[@class='textCenter bold' and contains(text(), 'Respiratory')]]"},
+			{"Endocrine", "//table[@id='mainTable' and tbody//tr/th[@class='textCenter bold' and contains(text(), 'Endocrine')]]"},
+			{"Cardiac Status", "//table[@id='mainTable' and tbody//tr/th[@class='textCenter bold' and contains(text(), 'Cardiovascular')]]"},
+			{"Elimination Status", "//table[@id='mainTable' and tbody//tr/th[@class='textCenter bold' and contains(text(), 'GU')]]"},
+			{ "Nutrition","//table[@id='mainTable' and tbody//tr/th[@class='textCenter bold' and contains(text(), 'Nutrition')]]"},
+			{ "Neuro/Emotional/Behavioral Status","//div[@id='oasisNeuroemostatus']//table[@id='mainTable']"},
+			{ "ADL/IADLs","//table[@id='mainTable' and tbody//tr/th[@class='textCenter bold' and contains(text(), 'Musculoskeletal')]]"}
+
+		};
+
+			return TitleAndPathDic;
+
+
+		}
+
+		public static IDictionary<string, string> HiddenMap()
+		{
+			IDictionary<string, string> HiddenMap = new Dictionary<string, string>();
+
+			HiddenMap.Add("M2250_PLAN_SMRY_DBTS_FT_CARE", "M2401_INTRVTN_SMRY_DBTS_FT");
+			HiddenMap.Add("M2250_PLAN_SMRY_FALL_PRVNT", "M2401_INTRVTN_SMRY_FALL_PRVNT");
+			HiddenMap.Add("M2250_PLAN_SMRY_DPRSN_INTRVTN", "M2401_INTRVTN_SMRY_DPRSN");
+			HiddenMap.Add("M2250_PLAN_SMRY_PAIN_INTRVTN", "M2401_INTRVTN_SMRY_PAIN_MNTR");
+			HiddenMap.Add("M2250_PLAN_SMRY_PRSULC_PRVNT", "M2401_INTRVTN_SMRY_PRSULC_PRVN");
+			HiddenMap.Add("M2250_PLAN_SMRY_PRSULC_TRTMT", "M2401_INTRVTN_SMRY_PRSULC_WET");
+
+
+			return HiddenMap;
+
+		}
+
+
+
+	}
+}
---
+++ KinserLib/Data/DerivedAnswers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinserLib.Data
+{
+
+	public class DerivedAnswersList
+	{
+
+		public DerivedAnswersList()
+		{
+			DerivedAnswers = new List<Data.DerivedAnswers>();
+		}
+		public List<DerivedAnswers> DerivedAnswers { get; set; }
+	}
+	public class DerivedAnswers
+	{
+
+		public DerivedAnswers()
+		{
+			SelectedCheckBoxes = new List<int>();
+		}
+		public bool IsMultiChoise { get; set; }
+
+		public bool IsTextArea { get; set; }
+
+		public string Text { get; set; }
+		public bool AnswerCaptured { get; set; }
+
+		public int SelectedRadioButtonOption { get; set; }
+
+		public int SelectedOption { get; set; }
+		public string Question { get; set; }
+
+		public List<int> SelectedCheckBoxes { get; set; }
+
+
+
+	}
+}
